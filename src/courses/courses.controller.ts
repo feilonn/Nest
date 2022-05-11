8835a8ac67bb812157b9951881ec2d39ec3afe11
Raul Correia <raulvinicius245@gmail.com>
2022-05-11 09:53:29 -0300

    Enviando dados no body da requisição.

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
@@ -12,4 +12,10 @@ export class CoursesController {
     findOne(@Param('id') id:string) {
         return `Curso #${id}`;
     }
+
+    @Post()
+    create(@Body('nome') body) {
+        return body;
+    }
+
 }
