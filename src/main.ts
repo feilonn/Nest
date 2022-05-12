33dce1c4cdb12c928d961a226b74b4e7c4c9d56b
Raul Correia <raulvinicius245@gmail.com>
2022-05-12 11:01:48 -0300

    Adicionado atributos para o ValidationPipe.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, //Retira do objeto da requisição os atributos que não constam no DTO
+    forbidNonWhitelisted: true, //Não permite que sejam enviadas informações não listadas no DTO
+    transform: true, //Define que o objeto enviando nas requests seja do tipo do DTO
+  })),
   await app.listen(3000);
 }
 bootstrap();
